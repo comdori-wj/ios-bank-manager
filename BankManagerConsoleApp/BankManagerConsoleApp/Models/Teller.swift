1f2f731f107d3b014b67ab015af4c151371935da
YunSwain <destap@naver.com>
2023-11-05 20:08:30 +0900

    Step3 연습

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Models/Teller.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+final class Teller {
+    let workType: WorkType
+    
+    init(workType: WorkType) {
+        self.workType = workType
+    }
+    
+    func service(to customer: Customer, completion: @escaping () -> Void) {
+        InterfaceMessage.printStartWork(customer: customer)
+        Thread.sleep(forTimeInterval: customer.workType.timeCost)
+        InterfaceMessage.printCompleteWork(customer: customer)
+        completion()
+    }
+}
